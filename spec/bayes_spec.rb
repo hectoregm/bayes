6b164c00e5ad39b738c2f4bd70690431e8abf0cb
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-05-06 21:26:53 -0500

    Redo rspec config.

--- spec/bayes_spec.rb
+++ spec/bayes_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 describe Bayes do
-  it 'create' do
-    expect(Bayes.create).to be_an_instance_of(Bayes::Network)
-  end
-end
 
+  describe "#create" do
+    it 'creates a new bayesian network' do
+      expect(Bayes.create).to be_an_instance_of(Bayes::Network)
+    end
+  end
+
+end
--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 describe Bayes::Network do
-  it 'create a new bayes network' do
-    expect(Bayes::Network.new).to be_an_instance_of(Bayes::Network)
+
+  describe "#new" do
+    it 'creates a new bayes network' do
+      expect(Bayes::Network.new).to be_an_instance_of(Bayes::Network)
+    end
   end
+
 end
